122401a07ab63996cbb1b5fadda8e4dc24170ab1
nedu'm <davidsonchinedum@gmail.com>
2022-07-06 08:46:48 +0100

    fixed map function error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { dev }) {
+    config.module.rules.push({
+      test: /\.(png|svg|jpg|gif)$/,
+      use: ['file-loader'],
+    });
+    return config;
+  }
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home({ artists }) {
   console.log(artists);
@@ -10,13 +11,14 @@ export default function Home({ artists }) {
         <meta name="keywords" content="spotify artists" />
       </Head>
 
-      {/* <div>
-        {
-          artists.map(artist => {
-            return (<p key={artist.id}>{artist}</p>)
-          })
-        }
-      </div> */}
+      {Object.keys(artists).map((artist) => {
+        return (
+          <div key={artist.toString()}>
+            <h1 >{artist.name}</h1>
+            <p>{artist.followers}</p>
+          </div>
+        );
+      })}
     </>
   );
 }
@@ -25,13 +27,14 @@ export const getStaticProps = async () => {
   const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '293967d804msh11125f294586c48p1cc50ajsnc41d7db86e92',
+      'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
       'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
     }
   };
 
   const res = await fetch('https://spotify23.p.rapidapi.com/artists/?ids=3wcj11K77LjEY1PkEazffa%2C1E5hfn5BduN2nnoZCJmUVG%2C1fYVmAFB7sC7eDoF3mJXla%2C3tVQdUvClmAT7URs9V3rsp%2C3TVXtAsR1Inumwj472S9r4%2C1RyvyyTE3xzB2ZywiAwp0i%2C5f7VJjfbwm532GiveGC0ZK%2C2LIk90788K0zvyj2JJVwkJ%2C2YZyLoL8N0Wb9xBt1NhZWg%2C6jGMq4yGs7aQzuGsMgVgZR%2C7bXgB6jMjp9ATFy66eO08Z%2C12kjvw4e3gLp6qVHO65n7W%2C20sxb77xiYeusSH8cVdatc%2C14CHVeJGrR5xgUGQFV5BVM', options);
   const artists = await res.json();
+
 
   return {
     props: {
