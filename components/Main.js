406c76198de8e2ca29b128ec2a2a7047ab05a27a
nedu'm <davidsonchinedum@gmail.com>
2022-07-09 22:49:06 +0100

    made changes to navbar

--- components/Main.js
+++ components/Main.js
@@ -15,12 +15,12 @@ const Main = () => {
         <div className={styles.frame}>
           <div className={styles.frame__title_wrapper}>
             <h1 className={styles.frame__title}>Spotify Random Artists</h1>
-            <p className={styles.frame__tagline}>Inspired by this <a className={styles.link} href="https://dribbble.com/shots/16174007-Kinetic-Typography">shot</a></p>
+            {/* <p className={styles.frame__tagline}>Inspired by this <a className={styles.link} href="https://dribbble.com/shots/16174007-Kinetic-Typography">shot</a></p> */}
           </div>
           {/* <div className={styles.frame__author}><a className={styles.link__alt} href="https://github.com/nedu-m">@Nedum</a></div> */}
           <nav className={styles.frame__links}>
-            <a href='#'> Home</a>
-            <a href='#'> About</a>
+            <a href='#'> Playlists</a>
+            <a href='#'> Podcasts</a>
             <a href='#'> Search</a>
           </nav>
         </div>
