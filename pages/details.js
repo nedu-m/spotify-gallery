2eb0ecea648d1336a83c7c64fc5c9bc283725ce0
nedu'm <davidsonchinedum@gmail.com>
2022-07-12 17:23:57 +0100

    multiple api requests for diff artists

--- pages/details.js
+++ pages/details.js
@@ -3,8 +3,10 @@ import Image from 'next/image'
 import detailStyles from "../styles/Details.module.css"
 import { CgArrowLongLeft } from 'react-icons/cg';
 
-export default function Details({ artist }) {
+export default function Details({ artist, artist2, artist3 }) {
   console.log(artist)
+  console.log(artist2)
+  console.log(artist3)
 
   return (
     <>
@@ -24,7 +26,7 @@ export default function Details({ artist }) {
   )
 }
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   const options = {
     method: 'GET',
     headers: {
@@ -33,13 +35,28 @@ export const getServerSideProps = async () => {
     }
   };
 
-  const res = await fetch('https://spotify23.p.rapidapi.com/artist_overview/?id=3wcj11K77LjEY1PkEazffa', options);
-  const data = await res.json();
-  const { artist } = data.data
+  // const res = await fetch('https://spotify23.p.rapidapi.com/artist_overview/?id=3wcj11K77LjEY1PkEazffa', options);
+  // const data = await res.json();
+  // const { artist } = data.data
+
+  const urls = [
+    'https://spotify23.p.rapidapi.com/artist_overview/?id=3wcj11K77LjEY1PkEazffa',
+    'https://spotify23.p.rapidapi.com/artist_overview/?id=3TVXtAsR1Inumwj472S9r4',
+    'https://spotify23.p.rapidapi.com/artist_overview/?id=3tVQdUvClmAT7URs9V3rsp',
+  ]
+
+  const responses = await Promise.all(urls.map(url => fetch(url, options)))
+  const data = await Promise.all(responses.map(res => res.json()));
+  const { artist } = data[0].data
+  const { artist: artist2 } = data[1].data
+  const { artist: artist3 } = data[2].data
+
 
   return {
     props: {
-      artist: artist
-    },
-  };
+      artist: artist.profile,
+      artist2: artist2.profile,
+      artist3: artist3.profile,
+    }
+  }
 };
