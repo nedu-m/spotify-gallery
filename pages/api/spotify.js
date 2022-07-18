9099e821bcc55a014646bcd29f94b966777ad417
nedu'm <davidsonchinedum@gmail.com>
2022-07-18 15:28:23 +0100

    updated api

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -8,7 +8,7 @@ const {
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const ENDPOINT = `https://api.spotify.com/v1/artists?ids=3tVQdUvClmAT7URs9V3rsp%2C3wcj11K77LjEY1PkEazffa%2C3TVXtAsR1Inumwj472S9r4%2C2YZyLoL8N0Wb9xBt1NhZWg%2C75VKfyoBlkmrJFDqo1o2VY%2C1fYVmAFB7sC7eDoF3mJXla%2C1RyvyyTE3xzB2ZywiAwp0i%2C2IK173RXLiCSQ8fhDlAb3s`;
-const GET_PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/me/playlists?limit=6`;
+const GET_PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/me/playlists?limit=8`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
