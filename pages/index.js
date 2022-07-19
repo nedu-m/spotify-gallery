ce8327056b0f31e9bae09e933e6e6930235deba5
nedu'm <davidsonchinedum@gmail.com>
2022-07-19 03:28:47 +0100

    error styles

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export async function getStaticProps() {
 export default function Home({ artists, error }) {
 
   if (error) {
-    return <div className={styles.error}>{error}</div>;
+    return <div className={styles.error}>{error} 😪</div>;
   }
 
   return (
